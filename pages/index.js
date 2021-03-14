af2cf461fc686a42d547a1c1f23ac16fd69ef19f
Andres Maza <opandre123@gmail.com>
2021-03-14 10:22:30 -0400

    initial commit course

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import Link  from 'next/link'
+import AppLayout from '../styles/components/AppLayout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>devter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AppLayout>
+      <h1 className={styles.title}>
+          <Link href="/">
+             <a >
+               devter
+             </a>           
+          </Link>
+        </h1>
+        <nav>
+          <Link href="/timeline">
+             <a >
+               timeline
+             </a>          
+          </Link>
+        </nav>
+
+      </AppLayout>
+
+      <style jsx>
+        {`
+          nav{
+            font-size:24px;
+            text-align:center:
+            }
+        `}
+      </style>
+
+    </div>
+  )
+}
