5b2b10a5c8b60c30180e639ec037722bbbe8030f
Andres Maza <opandre123@gmail.com>
2021-03-14 10:46:31 -0400

    edit title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>devter</title>
+        <title>devter rama two</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
